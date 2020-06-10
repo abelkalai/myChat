cef96a45d0f82239eedac8d36a8b6f4dee1c577d
abelkalai <43824518+abelkalai@users.noreply.github.com>
2020-06-10 19:30:24 -0400

    Changed WSS link clientside

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -19,7 +19,7 @@ const domainName = window.location.hostname;
 const port= 4000
 
 const wsLink = new WebSocketLink({
-  uri: `wss://${domainName}:${port}/graphql`,
+  uri: `ws://${domainName}:${port}/graphql`,
   options: {
     reconnect: true,
     lazy: true
